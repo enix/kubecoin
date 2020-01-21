bc64c6d64b2a2a0b7bcb3cf2f37b2946d0f82c4a
Julien Girardin <julien.girardin@enix.fr>
2020-01-21 17:04:36 +0100

    Add prometheus exporter to hasher

--- dockerfiles/hasher/hasher.rb
+++ dockerfiles/hasher/hasher.rb
@@ -1,18 +1,30 @@
 require 'digest'
 require 'sinatra'
 require 'socket'
+require 'prometheus/client'
+require 'prometheus/client/formats/text'
 
 set :bind, '0.0.0.0'
 set :port, 80
+
+prometheus = Prometheus::Client.registry
+http_requests = prometheus.counter(:http_requests, docstring: 'A counter of HTTP requests made')
+hash_size = prometheus.counter(:hash_size, docstring: 'A counter of total size hashed')
 
 post '/' do
     # Simulate a bit of delay
     sleep 0.1
     content_type 'text/plain'
-    "#{Digest::SHA2.new().update(request.body.read)}"
+    body = request.body.read
+    http_requests.increment
+    hash_size.increment(:by => body.length)
+    "#{Digest::SHA2.new().update(body)}"
 end
 
 get '/' do
     "HASHER running on #{Socket.gethostname}\n"
 end
 
+get '/metrics' do
+  "#{Prometheus::Client::Formats::Text::marshal(prometheus)}"
+end
